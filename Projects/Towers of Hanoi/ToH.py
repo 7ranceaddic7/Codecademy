f067729c2434bc84c65df7fc3a5af3c8607b64fa
Scott <4062616+7ranceaddic7@users.noreply.github.com>
2020-11-15 13:23:46 -0800

    complete

--- Projects/Towers of Hanoi/ToH.py
+++ Projects/Towers of Hanoi/ToH.py
@@ -30,15 +30,52 @@ print("\nThis game can be solved in {0} moves.".format(num_optimal_moves))
 #Get User Input
 def get_input():
   choices = [stack.get_name()[0] for stack in stacks] 
-  choice = input("Select Stack to move: " + str(choices)).format(choices=choices)
+  # choice = input("Select Stack to move: " + str(choices)).format(choices=choices)
   while True:
-    input("Select Stack to move: " + str(choices) + " ").format(choices=choices)
+    user_input = input("Select Stack to move: " + str(choices) + " ").format(choices=choices).upper()
 
     for i in range(len(stacks)):
       name = stacks[i].get_name()
       letter = choices[i]
       print("Enter {0} for {1}".format(letter, name))
 
-    user_input = ""
+    if user_input in choices:
+      for i in range(len(stacks)):
+        if user_input == choices[i]:
+          print(stacks[i].get_name() + " Stack selected.")
+          return stacks[i]
 
 #Play the Game
+num_user_moves = 0
+
+while right_stack.get_size() != num_disks:
+  print("Move Count: " + str(num_user_moves))
+  print("Remaining Optimal Moves: " + str(num_optimal_moves - num_user_moves))
+  print("\n...Current Stacks...")
+  left_stack.print_items()
+  middle_stack.print_items()
+  right_stack.print_items()
+
+  while True:
+
+    print("\nWhich stack do you want to move from?\n")
+    from_stack = get_input()
+    if from_stack.get_size() == 0:
+      print("\n\nInvalid move. Try again.")
+      break
+
+    print("\nWhich stack do you want to move to?\n")
+    to_stack = get_input()
+    if to_stack.get_size() == 0 or from_stack.peek() < to_stack.peek():
+      move_disk = from_stack.pop()
+      to_stack.push(move_disk)
+      num_user_moves += 1
+    else:
+      print( "/n/nInvalid move. Try again.")
+    break    
+  
+  if num_user_moves == num_optimal_moves:
+    print("*** Congratulations! ***")
+    print("You completed the game in the optimal number of moves.")
+  else:
+    print("\n\nYou completed the game in {0} moves, and the optimal number of moves is {1}".format(num_user_moves,num_optimal_moves))
