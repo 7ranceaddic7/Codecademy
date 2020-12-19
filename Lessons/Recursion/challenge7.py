34148aab901e8d142d47582b0d1d458267e65a72
Scott <4062616+7ranceaddic7@users.noreply.github.com>
2020-12-19 11:52:58 -0800

    update

--- Lessons/Recursion/challenge7.py
+++ Lessons/Recursion/challenge7.py
@@ -1,7 +1,37 @@
+# ITERATIVE CALL
 def depth(tree):
-  if no child:
-    return
+  result = 0
+  # our "queue" will store nodes at each level
+  queue = [tree]
+  # loop as long as there are nodes to explore
+  while queue:
+    # count the number of child nodes
+    level_count = len(queue)
+    for child_count in range(0, level_count):
+      # loop through each child
+      child = queue.pop(0)
+     # add its children if they exist
+      if child["left_child"]:
+        queue.append(child["left_child"])
+      if child["right_child"]:
+        queue.append(child["right_child"])
+    # count the level
+    result += 1
+  return result
 
+
+# RECURSIVE CALL
+def depth(tree):
+  if not tree:
+    return 0
+
+  left_depth = depth(tree["left_child"])
+  right_depth = depth(tree["right_child"])
+
+  if left_depth > right_depth:
+    return left_depth + 1
+  else:
+    return right_depth + 1
   
 
 # HELPER FUNCTION TO BUILD TREES
